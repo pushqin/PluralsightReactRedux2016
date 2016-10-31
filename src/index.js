113aceff9c0e5cb6850dc4f9d3aaf58b0624b6f3
alexgo <alexgo@payoneer.com>
2016-10-31 07:14:11 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+/**
+ * Created by alexgo on 10/10/2016.
+ */
+/* eslint-disable no-console */
+console.log('hi');
+
+
---
+++ src/index.test.js
@@ -0,0 +1,10 @@
+/**
+ * Created by alexgo on 10/11/2016.
+ */
+import expect from 'expect';
+
+describe('our first test', () => {
+    it('should pass', () => {
+        expect(true).toEqual(true);
+    });
+});
